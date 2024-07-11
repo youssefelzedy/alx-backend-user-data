806418681c6b1f15e2dcd148e11a1bc86a49636e
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-11 21:04:15 +0300

    final task

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,10 +3,24 @@
 Session Authentication
 """
 
+from typing import Dict
 from api.v1.auth.auth import Auth
+from datetime import datetime
+import uuid
 
 
 class SessionAuth(Auth):
     """Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        '''Create a session ID
+        '''
+
+        if user_id is None or type(user_id) is not str:
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
