9f161808b25a0b55fd5a2bd723dbded3e3c342d7
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-12 21:00:50 +0300

    final task

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -27,6 +27,9 @@ elif getenv('AUTH_TYPE') == 'session_auth':
 elif getenv('AUTH_TYPE') == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
 @app.errorhandler(404)
