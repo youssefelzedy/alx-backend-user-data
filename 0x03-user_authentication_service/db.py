cea89e7f2f5e94f887a13682ab1160549f9dd73d
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-18 18:41:52 +0300

    final task

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -28,12 +28,12 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
+
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
-        
+
         return new_user
