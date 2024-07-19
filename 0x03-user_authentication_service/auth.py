71cc94c94782fe5edabd0c8977d9017000bcf84e
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 11:39:51 +0300

    final task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -8,6 +8,7 @@ from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from typing import Union
 from user import User
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
