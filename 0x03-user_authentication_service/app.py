8d7fbd3ada2d19565e06e129610300fb29954c1b
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 12:13:39 +0300

    final task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -67,5 +67,25 @@ def login() -> str:
     return response
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    '''DELETE /sessions
+    Return: JSON payload
+    '''
+    session_id = request.cookies.get('session_id', None)
+
+    if session_id is None:
+        abort(403)
+
+    user = AUTH.get_user_from_session_id(session_id)
+
+    if user is None:
+        abort(403)
+
+    AUTH.destroy_session(user.id)
+
+    return redirect('/')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
