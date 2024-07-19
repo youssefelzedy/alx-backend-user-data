3631b4946c06a376d4cc0382f5f1f5a22525fad9
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 11:46:36 +0300

    final task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -41,5 +41,25 @@ def register() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login() -> str:
+    '''POST /sessions
+    Return: JSON payload
+    '''
+    try:
+        email = request.form.get('email')
+        password = request.form.get('password')
+    except KeyError:
+        abort(400)
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if not session_id:
+            abort(401)
+        return jsonify({"email": email, "message": "logged in"})
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
