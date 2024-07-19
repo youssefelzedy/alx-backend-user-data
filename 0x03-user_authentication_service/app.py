78676e3470de8d430b29bfb7b68646292c7c7ca6
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 12:34:33 +0300

    final task

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -123,5 +123,25 @@ def get_reset_password_token() -> str:
     return jsonify({"email": email, "reset_token": token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password() -> str:
+    '''PUT /reset_password
+    Return: JSON payload
+    '''
+    try:
+        email = request.form.get('email')
+        reset_token = request.form.get('reset_token')
+        new_password = request.form.get('new_password')
+    except KeyError:
+        abort(400)
+
+    try:
+        AUTH.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
