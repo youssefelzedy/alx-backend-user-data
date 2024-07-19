edd3d005a9f1f087f0e09d2f453b6eef3eec1fa5
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 11:39:14 +0300

    final task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -15,6 +15,11 @@ def _hash_password(password: str) -> bytes:
     '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+def _generate_uuid() -> str:
+    '''Generate a uuid
+    '''
+    return str(uuid.uuid4())
+
 
 class Auth:
     """Auth class to interact with the authentication database.
