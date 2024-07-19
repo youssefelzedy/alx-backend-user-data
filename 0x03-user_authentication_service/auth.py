f85f4d293a923163c63e90eb5e0b16e4dc9627d0
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 11:37:52 +0300

    final task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -32,3 +32,13 @@ class Auth:
             return self._db.add_user(email, _hash_password(password))
         else:
             raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate a user login
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(),
+                              user.hashed_password)
