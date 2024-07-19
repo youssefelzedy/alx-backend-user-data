e5ae24d108e25948c07b270a0f713cde2c006765
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 11:42:35 +0300

    final task

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -49,3 +49,14 @@ class Auth:
             return False
         return bcrypt.checkpw(password.encode(),
                               user.hashed_password)
+
+    def create_session(self, email: str) -> str:
+        """Create a new session
+        """
+        session_id = _generate_uuid()
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, session_id=session_id)
+        except NoResultFound:
+            return None
+        return session_id
