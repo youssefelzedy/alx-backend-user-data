f74f9bb737c824b047b9860c00953d6c31ab23df
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-19 10:59:57 +0300

    final task

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+'''
+Main file
+'''
+
+from auth import Auth
+from flask import (Flask, jsonify, request, abort, redirect)
+
+app = Flask(__name__)
+auth = Auth()
+
+
+@app.route('/', methods=['GET'])
+def welcome() -> str:
+    '''GET /
+    Return: welcome message
+    '''
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
